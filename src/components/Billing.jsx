bb3c62c7df4b1dc0df1c17e596cebdd907d16a8e
Hrishikesh Deshmukh <59472106+HrishikeshDeshmukh@users.noreply.github.com>
2023-12-29 16:18:20 +0530

    Add existing project files prior to the push to GitHub.

---
+++ src/components/Billing.jsx
@@ -0,0 +1,35 @@
+import { apple, bill, google } from "../assets"
+import styles, { layout } from "../style"
+const Billing = () =>
+(
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={bill} alt="bill"
+        className="w-[100%] h-[100%] relative z-[5]" />
+
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%]
+      rounded-full white__gradient" />
+      <div className="absolute z-[0] -left-1/2 top-0 w-[50%] h-[50%]
+      rounded-full pink__gradient" />
+    </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={`${styles.heading2}`}>Easily control your <br className="sm:block hidden" /> billing & invoicing.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        A veniam assumenda odit unde perspiciatis aliquid sint voluptates porro asperiores id.
+      </p>
+
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img src={apple} alt="apple_play"
+          className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+
+        <img src={google} alt="google_play"
+          className="w-[128px] h-[42px] object-contain  cursor-pointer" />
+      </div>
+    </div>
+  </section>
+)
+
+
+export default Billing
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../style'
+
+const Button = () => {
+  return (
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10 rounded-[10px]`}>
+      Get Started
+    </button>
+  )
+}
+
+export default Button
